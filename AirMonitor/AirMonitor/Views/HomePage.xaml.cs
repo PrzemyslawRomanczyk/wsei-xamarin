b9d7523eb4d0db02c672e3a118cb5b738a0efa8d
PrzemyslawRomanczyk <49125731+PrzemyslawRomanczyk@users.noreply.github.com>
2020-05-31 16:47:47 +0200

    2.2

---
+++ AirMonitor/AirMonitor/Views/HomePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Windows.Input;
+
+namespace AirMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+        private readonly INavigation _navigation;
+        async void GoDetailsPage(object sender, EventArgs args)
+        {
+            await _navigation.PushAsync(new DetailsPage());
+        }
+
+    }
+}
